30dad68bbc8610c51c94c342ea3a39737dd1aebb
AnnaMarandola <annamarandola888@gmail.com>
2019-10-02 11:28:08 +0200

    wild wild quest

---
+++ server.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Camille'));
+console.log(chalk.red('Coralie'));
+console.log(chalk.green('Hélène'));
+console.log(chalk.yellow('Jean-Claude'));
